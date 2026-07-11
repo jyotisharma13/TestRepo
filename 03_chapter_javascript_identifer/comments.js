dbcdf7deefa6d04a2da75a2cb685444c2338a276
Jyoti Sharma <sharma.jyoti27april@gmail.com>
2026-07-11 15:47:27 +0200

    sigle and multiline comments

---
+++ 03_chapter_javascript_identifer/comments.js
@@ -0,0 +1,16 @@
+// This is single comment this will be ignore 
+// this line will be not executed
+
+/*
+ *  This is multi line
+ *  Author : jyoti sharma
+ *  Date : 11-Jul-2026
+ */
+
+/**
+ *  This is multi line
+ *  Author : Jyoti Sharma
+ *  Date : 14-Feb-2026
+ **/
+
+var name = "jyoti"; // this is single line comment
