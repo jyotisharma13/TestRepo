02c5e7ed0e22e60c4723a5ded01f71cf65608267
Jyoti Sharma <sharma.jyoti27april@gmail.com>
2026-07-17 01:25:14 +0200

    Add/update 16_Comparsion_Operator.js

--- 05_chapter_operators/16_Comparsion_Operator.js
+++ 05_chapter_operators/16_Comparsion_Operator.js
@@ -1,5 +1,12 @@
 let a=1;
 let b=2;
-
+5 == "5"    // true  (loose equality - compares value only)
+5 === "5"   // false (strict equality - compares value + type)
+5 != "5"    // false
+5 !== "5"   // true
+5 > 3       // true
+5 < 3       // false
+5 >= 5      // true
+5 <= 4      // false
  a==b;
 console.log("boolean",a==b);
