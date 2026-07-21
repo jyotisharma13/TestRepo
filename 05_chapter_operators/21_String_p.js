6d52cd5c790bf9554f1649405e094bd29b6b8d1b
Jyoti Sharma <sharma.jyoti27april@gmail.com>
2026-07-21 03:49:43 +0200

    Update workspace files

---
+++ 05_chapter_operators/21_String_p.js
@@ -0,0 +1,8 @@
+let s="hi there";
+s += " how are you?";
+console.log(s);
+console.log(s.length);
+console.log(s[0]);
+console.log(1,2,3,4,5,6,7,8,9,10,"hello"+"world");
+console.log("hello"+"world");
+console.log("hello","world");
